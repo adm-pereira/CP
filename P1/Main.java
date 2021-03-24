ea5ef5dc31b065ab77f3d61bc7471080519e1590
António Pereira <adm.pereira@campus.fct.unl.pt>
2021-03-24 20:20:35 +0000

    Consola

--- P1/Main.java
+++ P1/Main.java
@@ -3,11 +3,41 @@ import java.util.Scanner;
 
 public class Main {
 
-
+	private static int nPoints;
+	
 	public static void main(String[] args) {
-		System.out.println("Teste do GitHub!");
+		Scanner in = new Scanner(System.in);
+		
+		console(in);
 	}
 
+	private static void console(Scanner in) {
+		String line = in.nextLine();
+		String[] vLine = line.split(" ");
+		
+		switch(vLine[0]) {
+			case "approxPi":
+				nPoints = Integer.parseInt(vLine[1]);
+				monteCarlo();
+			break;
+			case "time":
+				nPoints = Integer.parseInt(vLine[2]);
+				monteCarlo();
+				timeStats();
+			break;
+			default:
+				System.out.println("Comando inválido!");
+			break;
+		}
+	}
+
+	private static void monteCarlo() {
+		//todo
+	}
+
+	private static void timeStats() {
+		//todo
+	}
 
 
 }
