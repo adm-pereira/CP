3ca9a91d64ee0e9cc0e1828195076bc5f3807665
António Pereira <adm.pereira@campus.fct.unl.pt>
2021-03-27 03:02:58 +0000

    restructure for using time command directly from shell

---
+++ P1/approxPi.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class approxPi {
+
+	private static int nPoints, radius, nInside;
+	private static float approxPi;
+	
+	public static void main(String[] args) {
+		nPoints = Integer.parseInt(args[0]);
+		nInside = 0;
+		radius = 1;
+		//console(in);
+		monteCarlo();
+	}
+
+	private static void monteCarlo() {
+		for (int i = 0; i < nPoints; i++) {
+			double[] point = new double[2];
+			// doesnt matter that the point is always positive
+			//because circunference is both x and y simetrical
+			Double x = Math.random();
+			Double y = Math.random();
+			point[0] = x; point[1] = y;
+			if (x*x + y*y <= radius*radius) {
+				//inside circle
+				nInside++;
+			} else {
+				//outside circle
+			}
+		}
+		float rho = ((float)nInside)/((float)nPoints);
+		approxPi = 4*rho;
+		System.out.println("Total Number of points: " + nPoints);
+		System.out.println("Points within the circle: " + nInside);
+		System.out.println("Pi estimation: "+ approxPi);
+	}
+
+
+}
